ab2406e1f7167ad57b1e927f7f2863c06a1e95c9
Benjamin Intal <bfintal@gmail.com>
2017-11-18 12:37:56 +0800

    Create non-extensible-example.php

---
+++ non-extensible-example.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Non-Extensible Example
+Description: A non-extensible example
+Version: 20171118
+Author: Benjamin Intal @bfintal
+Author URI: twitter.com/bfintal
+License: MIT License
+License URI: https://opensource.org/licenses/MIT
+*/
+
